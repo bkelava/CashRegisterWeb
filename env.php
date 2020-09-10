d19b24d765b80da70983dcb72ef49d03fa4ef44f
unknown <bozidar.kelava@gmail.com>
2020-09-11 00:10:20 +0200

    commit db changes

--- env.php
+++ env.php
@@ -3,8 +3,17 @@ namespace db;
 
 class DbConfig
 {
+    const HOST = "eu-cdbr-west-03.cleardb.net";
+    const USER = "be12b9f50d95e6";
+    const PASS = "58feb553";
+    const DB = "heroku_bc5f57a9a257445";
+}
+
+
+/*class DbConfig
+{
     const HOST = "localhost";
     const USER = "root";
     const PASS = "";
     const DB = "cashregister";
-}
+}/*
